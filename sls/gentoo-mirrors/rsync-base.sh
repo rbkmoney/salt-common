9dccdeea67ffbf4b09f6373be7ffbbcd8eeeca57
Antsiferov Grigory <azr@bakka.su>
2017-01-12 11:12:41 +0300

    sls/gentoo-mirrors/rsync-base.sh: add some nice logging of actions to this decade old script;

--- sls/gentoo-mirrors/rsync-base.sh
+++ sls/gentoo-mirrors/rsync-base.sh
@@ -9,8 +9,18 @@ if [ "${base}" == "rsync-base" ]; then
 fi
 source "/etc/rsync/${base}.conf"
 
-echo "Started update at" `date` >> "${LOGDIR}/${base}.log" 2>&1
-logger -t rsync "re-rsyncing the gentoo-amd64-packages"
-${RSYNC} ${OPTS} ${SRC} ${DST} >> "${LOGDIR}/${base}.log" 2>&1
+logger -t rsync "Updating the ${base} from ${SRC} to ${DST}"
+COMMAND="${RSYNC} ${OPTS} ${SRC} ${DST}"
+echo -e "-=- Started update at $(date --rfc-3339=seconds)\n" \
+     "-=- Sync command: ${COMMAND}" \
+     >> "${LOGDIR}/${base}.log" 2>&1
+${COMMAND} >> "${LOGDIR}/${base}.log" 2>&1
+ret=$?
+echo "-=- The End: $(date --rfc-3339=seconds)" >> "${LOGDIR}/${base}.log" 2>&1
+if [ $ret == 0 ]; then
+    logger -t rsync "Successfully updated the ${base}"
+else
+    logger -t rsync "Failed to update the ${base}, return code: ${ret}"
+fi
+exit $ret
 
-echo "End: "`date` >> "${LOGDIR}/${base}.log" 2>&1
